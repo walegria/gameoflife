7885ddb63e0ce04cefb6010822a3f259a79c5bca
Tim Schraepen <sch3lp@gmail.com>
2017-04-19 21:36:01 +0200

    added live and dead cells as neighbours

--- src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
+++ src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
@@ -6,6 +6,10 @@ public class GameOfLifeTest {
 
     @Test
     public void liveCellWithFewerThanTwoLiveNeighboursDies() throws Exception {
+        boolean liveCell = true;
+        boolean deadNeighbour1 = false;
+        boolean deadNeighbour2 = false;
+        boolean liveNeighbour = true;
 
     }
 }
