d27a670a548ae48623bd6e93c22d3eb6aa1b9e6d
Tim Schraepen <sch3lp@gmail.com>
2017-04-19 22:18:34 +0200

    using static imports for dead and live cells, because they should be separate instances

--- src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
+++ src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
@@ -5,43 +5,39 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static be.cegeka.gameoflife.Cell.dead;
+import static be.cegeka.gameoflife.Cell.live;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameOfLifeTest {
 
     @Test
     public void liveCellWithFewerThanTwoLiveNeighboursDies() throws Exception {
-        Cell dead = Cell.dead();
-        Cell live = Cell.live();
-
         List<List<Cell>> world = Arrays.asList(
-            Arrays.asList(dead, dead, live),
-            Arrays.asList(live, live, live),
-            Arrays.asList(dead, dead, live)
+            Arrays.asList(dead(), dead(), live()),
+            Arrays.asList(live(), live(), live()),
+            Arrays.asList(dead(), dead(), live())
         );
 
         assertThat(tick(world)).containsExactly(
-            Arrays.asList(dead, dead, live),
-            Arrays.asList(dead, live, live),
-            Arrays.asList(dead, dead, live)
+            Arrays.asList(dead(), dead(), live()),
+            Arrays.asList(dead(), live(), live()),
+            Arrays.asList(dead(), dead(), live())
         );
     }
 
     @Test
     public void liveCellWithTwoOrThreeLiveNeighboursLivesOnToTheNextGeneration() throws Exception {
-        Cell dead = Cell.dead();
-        Cell live = Cell.live();
-
         List<List<Cell>> world = Arrays.asList(
-            Arrays.asList(dead, dead, live),
-            Arrays.asList(live, live, live),
-            Arrays.asList(dead, dead, live)
+            Arrays.asList(dead(), dead(), live()),
+            Arrays.asList(live(), live(), live()),
+            Arrays.asList(dead(), dead(), live())
         );
 
         assertThat(tick(world)).containsExactly(
-            Arrays.asList(dead, dead, live),
-            Arrays.asList(dead, live, live),
-            Arrays.asList(dead, dead, live)
+            Arrays.asList(dead(), dead(), live()),
+            Arrays.asList(dead(), live(), live()),
+            Arrays.asList(dead(), dead(), live())
         );
     }
 
