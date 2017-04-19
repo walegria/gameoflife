653382b708576a49ec43984a53f9e34d7095ec1e
Tim Schraepen <sch3lp@gmail.com>
2017-04-19 22:04:39 +0200

    added equals and toString for easier comparison and readability in test output

--- src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
+++ src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,11 +32,47 @@ public class GameOfLifeTest {
         return world;
     }
 
+    @Test
+    public void cellIsEqualToAnotherCellWhenAliveIsTheSame() throws Exception {
+        assertThat(new Cell(true)).isEqualTo(new Cell(true));
+        assertThat(new Cell(false)).isEqualTo(new Cell(false));
+        assertThat(new Cell(true)).isNotEqualTo(new Cell(false));
+        assertThat(new Cell(false)).isNotEqualTo(new Cell(true));
+    }
+
+    @Test
+    public void cellsToString_WhenAliveIsTrue_ThenLive() throws Exception {
+        assertThat(new Cell(true).toString()).isEqualTo("live");
+    }
+
+    @Test
+    public void cellsToString_WhenAliveIsFalse_ThenDead() throws Exception {
+        assertThat(new Cell(false).toString()).isEqualTo("dead");
+    }
+
     private class Cell {
         private boolean alive;
 
         public Cell(boolean alive) {
             this.alive = alive;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Cell cell = (Cell) o;
+            return alive == cell.alive;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(alive);
+        }
+
+        @Override
+        public String toString() {
+            return alive ? "live" : "dead";
         }
     }
 }
