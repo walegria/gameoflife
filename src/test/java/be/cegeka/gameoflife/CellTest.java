550c18f935860b76b0c22d1ec23f02e2e221a35e
Tim Schraepen <sch3lp@gmail.com>
2017-04-19 22:21:50 +0200

    more static imports

--- src/test/java/be/cegeka/gameoflife/CellTest.java
+++ src/test/java/be/cegeka/gameoflife/CellTest.java
@@ -2,26 +2,28 @@ package be.cegeka.gameoflife;
 
 import org.junit.Test;
 
+import static be.cegeka.gameoflife.Cell.dead;
+import static be.cegeka.gameoflife.Cell.live;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CellTest {
 
     @Test
     public void cellIsEqualToAnotherCellWhenAliveIsTheSame() throws Exception {
-        assertThat(Cell.live()).isEqualTo(Cell.live());
-        assertThat(Cell.dead()).isEqualTo(Cell.dead());
-        assertThat(Cell.live()).isNotEqualTo(Cell.dead());
-        assertThat(Cell.dead()).isNotEqualTo(Cell.live());
+        assertThat(live()).isEqualTo(live());
+        assertThat(dead()).isEqualTo(dead());
+        assertThat(live()).isNotEqualTo(dead());
+        assertThat(dead()).isNotEqualTo(live());
     }
 
     @Test
     public void cellsToString_WhenAliveIsTrue_ThenLive() throws Exception {
-        assertThat(Cell.live().toString()).isEqualTo("live");
+        assertThat(live().toString()).isEqualTo("live");
     }
 
     @Test
     public void cellsToString_WhenAliveIsFalse_ThenDead() throws Exception {
-        assertThat(Cell.dead().toString()).isEqualTo("dead");
+        assertThat(dead().toString()).isEqualTo("dead");
     }
 
 }
