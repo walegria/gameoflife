ed69a2db9973dda5df6c4a024d41365050c5db6b
Tim Schraepen <sch3lp@gmail.com>
2017-04-19 21:48:15 +0200

    setup world to make sure liveNeighbour remains as is decided to use simple boolean to represent liveCells that are irrelevant to the cell I want to test made deadCell explicit

--- src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
+++ src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
@@ -12,15 +12,24 @@ public class GameOfLifeTest {
     @Test
     public void liveCellWithFewerThanTwoLiveNeighboursDies() throws Exception {
         boolean liveCell = true;
+        boolean deadCell = true;
         boolean deadNeighbour = false;
         boolean liveNeighbour = true;
 
-        List<Boolean> world = Arrays.asList(deadNeighbour, liveCell, liveNeighbour);
+        List<List<Boolean>> world = Arrays.asList(
+            Arrays.asList(deadNeighbour, deadNeighbour, true),
+            Arrays.asList(liveCell,      liveNeighbour, true),
+            Arrays.asList(deadNeighbour, deadNeighbour, true)
+        );
 
-        assertThat(tick(world)).containsExactly(false, false, true);
+        assertThat(tick(world)).containsExactly(
+            Arrays.asList(deadNeighbour, deadNeighbour, true),
+            Arrays.asList(deadCell,      liveNeighbour, true),
+            Arrays.asList(deadNeighbour, deadNeighbour, true)
+        );
     }
 
-    private List<Boolean> tick(List<Boolean> world) {
+    private List<List<Boolean>> tick(List<List<Boolean>> world) {
         return world;
     }
 }
