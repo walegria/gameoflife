2a79f2a2c3ccf3000359db5f7dff14b263a24d10
Tim Schraepen <sch3lp@gmail.com>
2017-04-19 21:33:58 +0200

    created test for first rule

---
+++ src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,11 @@
+package be.cegeka.gameoflife;
+
+import org.junit.Test;
+
+public class GameOfLifeTest {
+
+    @Test
+    public void liveCellWithFewerThanTwoLiveNeighboursDies() throws Exception {
+
+    }
+}
