e9ea5bf8fded22d1a21f10fc6e3fdf3c1bb35885
Tim Schraepen <sch3lp@gmail.com>
2017-04-19 21:37:58 +0200

    put cells in world; removed unnecessary other neighbour

--- src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
+++ src/test/java/be/cegeka/gameoflife/GameOfLifeTest.java
@@ -2,14 +2,17 @@ package be.cegeka.gameoflife;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GameOfLifeTest {
 
     @Test
     public void liveCellWithFewerThanTwoLiveNeighboursDies() throws Exception {
         boolean liveCell = true;
-        boolean deadNeighbour1 = false;
-        boolean deadNeighbour2 = false;
+        boolean deadNeighbour = false;
         boolean liveNeighbour = true;
 
+        List<Boolean> world = Arrays.asList(deadNeighbour, liveCell, liveNeighbour);
     }
 }
