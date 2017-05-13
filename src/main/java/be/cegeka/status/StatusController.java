0b70353333fe095cc4fdd1ff07c22f1f004db9c2
walegria <william.alegriamira@cegeka.com>
2017-05-13 19:01:50 +0200

    Remove clutter comment

--- src/main/java/be/cegeka/status/StatusController.java
+++ src/main/java/be/cegeka/status/StatusController.java
@@ -1,18 +1,9 @@
 package be.cegeka.status;
 
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
-/**
- * Created by fransg on 02/12/15.
- */
 @RestController
 @RequestMapping(value = "/status")
 public class StatusController {
@@ -22,5 +13,4 @@ public class StatusController {
     public String getApplicationName(){
         return "{\"name\": \"Game of Life\"}";
     }
-
 }
