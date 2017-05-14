33f9a69e2dcee4bfd831a03bed97bd3c2cf82794
walegria <william.alegria.mira@cegeka.com>
2017-05-14 17:55:11 +0200

    add equal methods to cell and position

--- src/main/java/be/cegeka/gameoflife/domain/Cell.java
+++ src/main/java/be/cegeka/gameoflife/domain/Cell.java
@@ -7,4 +7,19 @@ public class Cell {
     public Cell(Position position) {
         this.position = position;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell cell = (Cell) o;
+
+        return position != null ? position.equals(cell.position) : cell.position == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return position != null ? position.hashCode() : 0;
+    }
 }
--- src/main/java/be/cegeka/gameoflife/domain/Position.java
+++ src/main/java/be/cegeka/gameoflife/domain/Position.java
@@ -1,6 +1,27 @@
 package be.cegeka.gameoflife.domain;
 
 public class Position {
+    private int x,y;
+
     public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
     }
 }
