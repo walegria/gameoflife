6750fc074f61cc453ce55c3a06d37f1b223efc29
walegria <william.alegriamira@cegeka.com>
2017-05-15 18:48:50 +0200

    Refactor method to get count vertical neighbours and the position of the vertical neighbours

--- src/main/java/be/cegeka/gameoflife/domain/Board.java
+++ src/main/java/be/cegeka/gameoflife/domain/Board.java
@@ -28,12 +28,21 @@ public class Board {
 
     public int neighbours(Cell cell) {
         int neigboursCount = 0;
+        neigboursCount = getVerticalNeigboursCount(cell, neigboursCount);
+
+        return neigboursCount;
+    }
+
+    private int getVerticalNeigboursCount(Cell cell, int neigboursCount) {
         for (int verticalNeighbours = -1; verticalNeighbours <= 1; verticalNeighbours += 2) {
-            if (alives.contains(new Cell(new Position(cell.getPosition().getX(), cell.getPosition().getY() + verticalNeighbours)))) {
+            if (alives.contains(new Cell(getPositionOfVerticalNeighbour(cell, verticalNeighbours)))) {
                 neigboursCount++;
             }
         }
-
         return neigboursCount;
+    }
+
+    private Position getPositionOfVerticalNeighbour(Cell cell, int verticalValue){
+        return new Position(cell.getHorizontalPositionValue(), cell.getVerticalPositionValue() + verticalValue);
     }
 }
