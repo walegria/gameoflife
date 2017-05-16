7bbac4f9c3a4f0a6b42ae349b9374c3311fc0b13
walegria <william.alegriamira@cegeka.com>
2017-05-16 19:07:35 +0200

    Added both Horizontal and Diagonal neighboars to countNeighbour method

--- src/main/java/be/cegeka/gameoflife/domain/Board.java
+++ src/main/java/be/cegeka/gameoflife/domain/Board.java
@@ -11,16 +11,40 @@ public class Board {
         alives = new HashSet<Cell>(Arrays.asList(cells));
     }
 
-    public int neighbours(Cell cell) {
-        int neigboursCount = 0;
-        neigboursCount = getVerticalNeigboursCount(cell, neigboursCount);
+    public int cellNeighbours(Cell cell) {
+        int neigboursCount = getVerticalNeigboursCount(cell);
+        neigboursCount += getHorizontalNeighboursCount(cell);
+        neigboursCount += getDiagonalNeighboursCount(cell);
 
         return neigboursCount;
     }
 
-    private int getVerticalNeigboursCount(Cell cell, int neigboursCount) {
-        for (int verticalNeighbours = -1; verticalNeighbours <= 1; verticalNeighbours += 2) {
-            if (alives.contains(new Cell(getPositionOfVerticalNeighbour(cell, verticalNeighbours)))) {
+    private int getDiagonalNeighboursCount(Cell cell) {
+        int neigboursCount = 0;
+        for (int neighbourHorizontal = -1; neighbourHorizontal <= 1; neighbourHorizontal += 2) {
+            for (int neighbourVertical = -1; neighbourVertical <=1; neighbourVertical += 2){
+                if (alives.contains(new Cell(getPositionOfDiagonalNeighbour(cell, neighbourHorizontal, neighbourVertical)))) {
+                    neigboursCount++;
+                }
+            }
+        }
+        return neigboursCount;
+    }
+
+    private int getHorizontalNeighboursCount(Cell cell) {
+        int neigboursCount = 0;
+        for (int neighbour = -1; neighbour <= 1; neighbour += 2) {
+            if (alives.contains(new Cell(getPositionOfHorizontalNeighbour(cell, neighbour)))) {
+                neigboursCount++;
+            }
+        }
+        return neigboursCount;
+    }
+
+    private int getVerticalNeigboursCount(Cell cell) {
+        int neigboursCount = 0;
+        for (int neighbour = -1; neighbour <= 1; neighbour += 2) {
+            if (alives.contains(new Cell(getPositionOfVerticalNeighbour(cell, neighbour)))) {
                 neigboursCount++;
             }
         }
@@ -30,4 +54,15 @@ public class Board {
     private Position getPositionOfVerticalNeighbour(Cell cell, int verticalValue){
         return new Position(cell.getHorizontalPositionValue(), cell.getVerticalPositionValue() + verticalValue);
     }
+
+    private Position getPositionOfHorizontalNeighbour(Cell cell, int horizontalValue) {
+        return new Position(cell.getHorizontalPositionValue()+ horizontalValue, cell.getVerticalPositionValue());
+    }
+
+    private Position getPositionOfDiagonalNeighbour(Cell cell, int horizontalValue, int verticalValue) {
+        return new Position(cell.getHorizontalPositionValue()+ horizontalValue, cell.getVerticalPositionValue()+verticalValue);
+
+    }
+
+
 }
--- src/test/java/be/cegeka/gameoflife/domain/BoardTest.java
+++ src/test/java/be/cegeka/gameoflife/domain/BoardTest.java
@@ -1,5 +1,6 @@
 package be.cegeka.gameoflife.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,21 +8,49 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BoardTest {
 
     private Board board;
+    private Cell cellZero;
+
+    @Before
+    public void setup() {
+        cellZero = new Cell(new Position(0, 0));
+    }
 
     @Test
     public void boardWithOnlyOneCell_ShouldHaveNoNeighbours(){
-        Cell cell = new Cell(new Position(0,0));
-        board =  new Board(cell);
+        board =  new Board(cellZero);
 
-        assertThat(board.neighbours(cell)).isEqualTo(0);
+        assertThat(board.cellNeighbours(cellZero)).isEqualTo(0);
     }
 
     @Test
-    public void boardWithVerticalAdjacentCells_ShouldOnlyCountVerticalNeighbours(){
-        Cell cellOne = new Cell(new Position(0,0));
-        Cell cellTwo = new Cell(new Position(0,1));
+    public void boardCellWithVerticalAdjacentCells_ShouldOnlyCountVerticalNeighbours(){
+        Cell cellAdjacentVertical = new Cell(new Position(0,1));
 
-        board = new Board(cellOne,cellTwo);
-        assertThat(board.neighbours(cellOne)).isEqualTo(1);
+        board = new Board(cellZero,cellAdjacentVertical);
+        assertThat(board.cellNeighbours(cellZero)).isEqualTo(1);
     }
+
+    @Test
+    public void boardCellWithHorizontalAdjacentCells_ShouldOnlyCountHorizontalNeighbours(){
+        Cell cellAdjacentHorizontal = new Cell(new Position(1,0));
+        Cell cellAdjacentHorizontalTwo = new Cell(new Position(-1,0));
+
+        board = new Board (cellZero,cellAdjacentHorizontal,cellAdjacentHorizontalTwo);
+        assertThat(board.cellNeighbours(cellZero)).isEqualTo(2);
+    }
+
+    @Test
+    public void boardCellWithAdjacentCells_ShouldReturnAllNeighbours(){
+        Cell cellAdjacentHorizontal = new Cell(new Position(1,0));
+        Cell cellAdjacentHorizontalTwo = new Cell(new Position(-1,0));
+        Cell cellAdjacentVertical = new Cell(new Position(0,1));
+        Cell cellDiagonalTopLeft = new Cell (new Position(-1,1));
+        Cell cellDiagonalBottomLeft = new Cell (new Position(-1,-1));
+
+        board = new Board (cellZero,cellAdjacentHorizontal,cellAdjacentHorizontalTwo,cellAdjacentVertical,cellDiagonalTopLeft,cellDiagonalBottomLeft);
+        assertThat(board.cellNeighbours(cellZero)).isEqualTo(5);
+    }
+
+
+
 }
